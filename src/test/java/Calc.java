79408f39f289b7811ea3d9faebc3400a1f9c2e49
mr.Boogie <zik2004@mail.ru>
2022-06-25 00:32:42 +0300

    add other operations

--- src/test/java/Calc.java
+++ src/test/java/Calc.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 
 
 /**
@@ -8,12 +9,46 @@ import java.io.InputStreamReader;
  */
 public class Calc {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
+
+        String answer;
+        do {
+            System.out.println("Выберите операцию :");
+            System.out.println("1 - Сложение");
+            System.out.println("2 - Вычитание");
+            System.out.println("3 - Умножение");
+            System.out.println("4 - Деление");
+
+            answer = br.readLine().trim();
+
+        }while (!(answer.equals("1") || answer.equals("2") || answer.equals("3") || answer.equals("4")));
 
         double firstParam = getDoubleFromConsole("Введите первое число");
         double secondParam = getDoubleFromConsole("Введите второе число");
 
-        System.out.printf("%.4f", sum(firstParam, secondParam));
+        double result = 0.0;
+        try {
+            switch (answer){
+                case "1" :
+                    result = sum(firstParam, secondParam);
+                    break;
+                case "2" :
+                    result = subtraction(firstParam, secondParam);
+                    break;
+                case "3" :
+                    result = multiplication(firstParam, secondParam);
+                    break;
+                case "4" :
+                        result = division(firstParam, secondParam);
+            }
+            System.out.printf("%.4f", result);
+
+        }catch (ArithmeticException e){
+            System.out.println("Делить на 0 нельзя");
+        }
+
     }
 
     /**
@@ -23,6 +58,34 @@ public class Calc {
      */
     public static Double sum(double a, double b){
         return a+b;
+    }
+
+    /**
+     * @param a first double param
+     * @param b second double param
+     * @return subtraction of two params
+     */
+    public static Double subtraction(double a, double b){
+        return a-b;
+    }
+
+    /**
+     * @param a first double param
+     * @param b second double param
+     * @return multiplication of two params
+     */
+    public static Double multiplication(double a, double b){
+        return a*b;
+    }
+
+    /**
+     * @param a first double param
+     * @param b second double param
+     * @return division of two params
+     */
+    public static Double division(double a, double b) throws ArithmeticException {
+        if (b==0) throw new ArithmeticException();
+        return a/b;
     }
 
     /**
